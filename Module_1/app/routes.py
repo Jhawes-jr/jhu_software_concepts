63b648ef68f6c8b391fa69cdcc303b34cb74ed72
Joe Hawes <jhawesjr@gmail.com>
2025-08-28 10:46:44 -0500

    Modified run.py, readme, and app/

---
+++ Module_1/app/routes.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def home():
+    home = {
+        "title": "Home",        
+        "Home":["Joe Hawes", "Student at Johns Hopkins University studying Artitificial Intelligence"]
+    }
+    return render_template("home.html", active_tab="home")
+
+@bp.route("/contact")
+def contact():
+    return render_template("contact.html", active_tab="contact")
+
+@bp.route("/projects")
+def projects():
+    module_1 = {
+        "github_url": "https://github.com/Jhawes-jr/jhu_software_concepts.git"
+    }
+    return render_template("projects.html", module_1=module_1, active_tab="projects")
--- Module_1/run.py
+++ Module_1/run.py
@@ -1,10 +1,8 @@
-from flask import Flask
+from app import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route('/') 
-def home():
-    return "Hello, World!"
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)    
+    # specifications for port and ip
+    app.run(host="0.0.0.0", port=8080, debug=True)  
