a3212d901ba8e0ce54c2c56d8bad3fb683d40432
Joe Hawes <jhawesjr@gmail.com>
2025-08-29 22:29:18 -0500

    changes to routes.py

--- Module_1/app/routes.py
+++ Module_1/app/routes.py
@@ -4,10 +4,6 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def home():
-    home = {
-        "title": "Home",        
-        "Home":["Joe Hawes", "Student at Johns Hopkins University studying Artitificial Intelligence"]
-    }
     return render_template("home.html", active_tab="home")
 
 @bp.route("/contact")
@@ -19,4 +15,4 @@ def projects():
     module_1 = {
         "github_url": "https://github.com/Jhawes-jr/jhu_software_concepts.git"
     }
-    return render_template("projects.html", module_1=module_1, active_tab="projects")
+    return render_template("projects.html", module_1=module_1,  active_tab="projects")
